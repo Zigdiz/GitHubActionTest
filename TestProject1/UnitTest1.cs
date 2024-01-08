4c169da57a97d9ac40d2e4f1e8daba4442a48b5a
Tuomas Reijonen <treijonen@live.fi>
2024-01-08 17:09:20 +0200

    Added unit tests

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Dummy test code
+            Assert.True(true);
+        }
+    }
+}
